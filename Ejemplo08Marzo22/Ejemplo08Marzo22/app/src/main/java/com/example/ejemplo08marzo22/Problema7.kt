c061fb7ebc454a4d22977020c9cc7292e1bce703
EzequillGuzman <moisespaz92@gmail.com>
2022-03-20 20:57:21 -0600

    Se agregan cambios

---
+++ Ejemplo08Marzo22/Ejemplo08Marzo22/app/src/main/java/com/example/ejemplo08marzo22/Problema7.kt
@@ -0,0 +1,18 @@
+package com.example.ejemplo08marzo22
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+
+class Problema7 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_problema7)
+
+
+
+    }
+}
